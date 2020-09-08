db0c4d311690f7831c2ab6afbae61b7a3cca5b86
Ikuma Sugawara <admjz1411@gmail.com>
2020-09-09 00:04:11 +0900

    パスワードリセット用バリデーションエラーメッセージを日本語化

---
+++ makeHabit/resources/lang/ja/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'パスワードをリセットしました。',
+    'sent' => 'パスワードリセット用のメールを送信しました。',
+    'token' => '予期せぬエラーが発生しました。初めから再度お試しください。',
+    'user' => "入力されたメールアドレスは登録されていません。",
+
+];
