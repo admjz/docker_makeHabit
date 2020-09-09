a641d7197bd3c08e10040e8ac76e0a9966547805
Ikuma Sugawara <admjz1411@gmail.com>
2020-09-09 23:39:43 +0900

    HabitPolicyのメソッド名復元＆deny時のメッセージ記述

--- makeHabit/app/Policies/HabitPolicy.php
+++ makeHabit/app/Policies/HabitPolicy.php
@@ -2,24 +2,14 @@
 
 namespace App\Policies;
 
-use App\Models\Habit;
 use App\Models\User;
+use App\Models\Habit;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class HabitPolicy
 {
     use HandlesAuthorization;
-
-    /**
-     * Determine whether the user can view any items.
-     *
-     * @param  \App\Models\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
 
     /**
      * Determine whether the user can view the item.
@@ -30,18 +20,9 @@ class HabitPolicy
      */
     public function view(User $user, Habit $habit)
     {
-        return $user->id === $habit->user_id;
-    }
-
-    /**
-     * Determine whether the user can create items.
-     *
-     * @param  \App\Models\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
+        return $user->id === $habit->user_id
+                ? Response::allow()
+                : Response::deny('This action is unauthorized.');
     }
 
     /**
@@ -51,44 +32,10 @@ class HabitPolicy
      * @param  \App\Item  $item
      * @return mixed
      */
-    public function update(User $user, Item $item)
+    public function update(User $user, Habit $habit)
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the item.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Item  $item
-     * @return mixed
-     */
-    public function delete(User $user, Item $item)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the item.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Item  $item
-     * @return mixed
-     */
-    public function restore(User $user, Item $item)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the item.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Item  $item
-     * @return mixed
-     */
-    public function forceDelete(User $user, Item $item)
-    {
-        //
+        return $user->id === $habit->user_id
+                ? Response::allow()
+                : Response::deny('This action is unauthorized.');
     }
 }
