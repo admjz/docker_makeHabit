98c67436f2c7c08c866f9a1a487f27c555ebbb7f
Ikuma Sugawara <admjz1411@gmail.com>
2020-09-09 22:37:33 +0900

    policiesプロパティにHabitPolicy用の記述追加

--- makeHabit/app/Providers/AuthServiceProvider.php
+++ makeHabit/app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Habit;
+use App\Policies\HabitPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        Habit::class     => HabitPolicy::class,
     ];
 
     /**
